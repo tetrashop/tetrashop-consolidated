c1b7e4814e5f8a33592fd672b6ab843b1ffff7a1
tetrashop <ramin.edjlal1359@gmail.com>
2025-10-20 16:29:36 +0330

    feat: add root index.html and simplify project structure

--- apps/web/out/_next/static/7eetLkV3A3_zKISS8d3Gd/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-3b5a862bec23d1c1.js"],"/_error":["static/chunks/pages/_error-cb6819b9c2fc7f62.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
